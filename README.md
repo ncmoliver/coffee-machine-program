698aff6a85591336dc447cc3fb5caf0aa1ae90fe
ncmoliver <mrmarquesoliver@gmail.com>
2024-07-09 19:11:32 -0400

    features upgraded: added admin menu & clear completed

--- README.md
+++ README.md
@@ -4,10 +4,25 @@ A dynamic coffee machine program that takes order, updates ingredients, processe
 
 ## Overview
 
+The coffee machine program is a program that works dynamically as a real world coffee machine. In this project I focused on agile coding practices and organization with the use of functions.
+
 ## Instructions
 
+There is only one project file **main.py**
+
+1. Clone Repository
+2. Must have Python installed
+3. Run the `main.py` file
+4. Enjoy your coffee break ☕️
+
 ## Features
+
+| Features        | Description                 |
+| --------------- | --------------------------- |
+| Dynamic Program | All program functionalities |
+| Paragraph       | Text                        |
 
 ## References
 
 [100 Days of Code: The Complete Pythong Pro Bootcamp](https://www.udemy.com/course/100-days-of-code/learn/lecture/19846936#overview) - Dr. Angela Yu
+[Markdown CheatSheet](https://www.markdownguide.org/cheat-sheet/)
--- main.py
+++ main.py
@@ -1,30 +1,6 @@
+from replit import clear
+from menu import MENU
 
-
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
 profit = 0
 resources = {
     "water": 300,
@@ -32,15 +8,13 @@ resources = {
     "coffee": 100,
 }
 
-
 def is_ingredient_available(ingredients):
     """Returns true if order can be made and false if ingredient is not available"""
     for item in ingredients:
-        if ingredients[item] >= resources[item]:
+        if ingredients[item] > resources[item]:
             print(f"Sorry there is not enough {item}")
             return False
     return True
-
 
 def process_coins():
     """Returns the total for the coins inserted"""
@@ -51,42 +25,78 @@ def process_coins():
     total += int(input("How many pennies?: ")) * 0.01
     return total
 
-
 def is_transaction_successful(money_received, drink_cost):
     """Return true if payment is accepted and false if insufficient"""
     if money_received >= drink_cost:
+        clear()
         change = round(money_received - drink_cost, 2)
-        print(f"Here is change: ${change} in change.")
+        print(f"Your Change ${change}\n")
         global profit
         profit += drink_cost
         return True
     else:
-        print(f"Sorry there is not enough, Amount refunded: {money_received}")
+        print(f"Sorry there is not enough money. Amount refunded: ${money_received}")
         return False
 
-def makeCoffee(drink_name, ingredients):
+def make_coffee(drink_name, ingredients):
     """Deduct the required ingredients from the resources"""
     for item in ingredients:
         resources[item] -= ingredients[item]
 
-    print(f"Here is your coffee: {drink_name} ☕️")
+    print(f"Here is your {drink_name} ☕️\n"
+          "Thank you for ordering.\nHave a Great Day!")
 
-
+machine_pass = "1098"
 machine_on = True
 
+menu_selection = ("Welcome to Your Coffee Break!\n\n"
+                  "Menu Selection\n"
+                  "-----------------------\n"
+                  "1. Espresso\n"
+                  "2. Latte\n"
+                  "3. Cappuccino\n"
+                  "4. Exit")
+admin_selection = ("Admin Screen\n"
+                   "1. Generate Machine Status Report\n"
+                   "2. Exit")
+
 while machine_on:
-    choice = input("What would you like? (espresso/latte/cappuccino): ")
-    if choice == "off":
+    clear()
+    print(menu_selection)
+    choice = input("What would you like to drink? (Enter admin pass for admin menu): ")
+
+    if choice == "4":
         machine_on = False
-    elif choice == "report":
-        print(f"Water: {resources['water']}ml")
-        print(f"Milk: {resources['milk']}ml")
-        print(f"Coffee: {resources['coffee']}ml")
-        print(f"Money: ${profit}")
-    else:
-        drink = MENU[choice]
+    elif choice == "1098":
+        print(admin_selection)
+        admin_choice = int(input("What would you like to perform? "))
+        if admin_choice == 1:
+            clear()
+            print(f"Water: {resources['water']}ml")
+            print(f"Milk: {resources['milk']}ml")
+            print(f"Coffee: {resources['coffee']}g")
+            print(f"Money: ${profit}")
+            input("Press any key to return to the main menu...")
+        else:
+            print("Have a good day")
+            machine_on = False
+    elif choice in ["1", "2", "3"]:
+        drink_map = {
+            "1": "espresso",
+            "2": "latte",
+            "3": "cappuccino"
+        }
+        drink_name = drink_map[choice]
+        drink = MENU[drink_name]
         if is_ingredient_available(drink["ingredients"]):
+            clear()
+            print("Customer Order Information")
+            print("-" * 30)
+            print(f"Customer Order: {drink_name}\nCost: ${drink['cost']}")
+            print("-" * 30)
             payment = process_coins()
             if is_transaction_successful(payment, drink["cost"]):
-                makeCoffee(choice, drink["ingredients"])
-
+                make_coffee(drink_name, drink["ingredients"])
+                machine_on = False
+    else:
+        print("Error: Invalid option.")
